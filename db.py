ac0df9ed9421cdfbc7544404ab433a1ed9b7958a
MukumovMuhammad <muhammad.bw.03@gmail.com>
2025-11-18 15:12:52 +0500

    made sign up post request, implemented in db and change websocket logic message handling for private message

--- db.py
+++ db.py
@@ -8,15 +8,31 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     username TEXT,
-    client_id INT
+    password TEXT
 )
 ''')
 
-def add_user(name, id):
+def add_user(name, password):
+    if is_user_exist(name):
+        return {"message": "user already exists", "status": False}
     cursor.execute('''
-           INSERT INTO users (username, client_id) VALUES (?,?)
-       ''', (name,id))
+           INSERT INTO users (username, password) VALUES (?,?)
+       ''', (name,password))
     connect.commit()
+    return {"message": "user added", "status": True, "id": get_user_id(name) }
+
+def check_password(username, password):
+    if not is_user_exist(username):
+        return False
+
+    
+    cursor.execute(" SELECT COUNT(*) FROM users WHERE username = ? AND password = ?", (username, password))
+    
+    # Fetch the result (a single number: 1 if match found, 0 otherwise)
+    count = cursor.fetchone()[0]
+    
+    return  count == 1
+     
 
 def get_usernames():
     cursor.execute(f'SELECT username FROM users')
@@ -28,6 +44,13 @@ def get_usernames():
             formated_usernames.append(i)
         return formated_usernames
     return []
+
+def get_user_id(username):
+    query = f"SELECT id FROM users WHERE {username} = ?"
+    cursor.execute(query, (username,))
+    user = cursor.fetchone()[0]
+    print("get a user by id data is - > ", user)
+    return user
 
 def get_a_user_by(type, value):
     query = f"SELECT * FROM users WHERE {type} = ?"
@@ -41,3 +64,13 @@ def delete_user(client_id):
     print(f"Got data {client_id}")
     cursor.execute("DELETE FROM users WHERE client_id = ?", (client_id,))
     connect.commit()
+
+def is_user_exist(user):
+    cursor = connect.cursor()
+    query = f"SELECT EXISTS(SELECT 1 FROM users WHERE name = ?)"
+    cursor.execute(query, (user,))
+
+    # fetchone() returns a tuple; the existence value is the first element
+    exists = cursor.fetchone()[0]
+    return exists == 1
+    
--- main.py
+++ main.py
@@ -9,25 +9,33 @@ app = FastAPI()
 class ConnectionManager:
 
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[WebSocket] = {}
 
     
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, user_id: int, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[user_id] = websocket
     
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-        
-        
-        
-    async def send_direct_message(self, message: object, websocket: WebSocket):
-        await websocket.send_json(message)
+    def disconnect(self, user_id: int):
+        if user_id in self.active_connections:
+            del self.active_connections[user_id]
     
-    async def broadcast_message(self, message: object, websocket: WebSocket):
-        for con in self.active_connections:
-            await con.send_json(message)
+    async def send_private_message(self, sender_id: int, receiver_id: int, text: str):
+
+        if receiver_id in self.active_connections:
+            websocket = self.active_connections[receiver_id]
+            await websocket.send_json({
+                "from": sender_id,
+                "text": text
+            })
+            
+        
+    
+    async def broadcast(self, message: dict):
+        for ws in self.active_connections.values():
+            await ws.send_json(message)
+    
 
 
 manager = ConnectionManager()
@@ -48,32 +56,41 @@ async def get():
     }
 
 
+@app.post("/sign_up")
+async def sign_up(username: str, password: str):
+    if db.is_user_exist(username):
+        return {"message" : "The user already exists", "status" : False}
+    return db.add_user(username, password)
+
 
 @app.post("/login")
-async def login_the_user(username: str, client_id: int):
+async def login_the_user(username: str, password: str):
     if username:
-        print("The user ", username, " with id : ", client_id,  " is added to db")
-        db.add_user(username, client_id)
-        return db.get_a_user_by("username", username)
+        if not db.is_user_exist(username):
+            return {"message" : "The user doesn't exist", "status" : False}
+        print("The user ", username, " with password : ", password,  " trying to login")
+        if db.check_password(username, password):
+            return db.get_a_user_by("username", username)
+        return {"message": "Password or username is incorrect!", "status" : True}
+        
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id : int):
-    await manager.connect(websocket)
-
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, user_id : int):
+    await manager.connect(user_id, websocket)
     try:
         while True:
             data = await websocket.receive_text() 
-            userdata = db.get_a_user_by("client_id", client_id)
-            print(f"The userdata is {userdata}")
-            print(f"well someone wrote {data}")
+            receiver_id = data["to"]
+            text = data["text"]
             # await manager.send_direct_message(f"you wrote {data}", websocket)
-            await manager.broadcast_message({"message": data, "username" : userdata[1],"client_id" : client_id}, websocket)
+            await manager.send_private_message(
+                sender_id=user_id,
+                receiver_id=receiver_id,
+                text=text
+                )
     except WebSocketDisconnect:
-        print(f"The user {client_id} is about to be deleted!")
-        db.delete_user(client_id)
-        manager.disconnect(websocket)
-        await manager.broadcast_message({"message": "has left the chat!", "username" : userdata[1],"client_id" : client_id}, websocket)
-
+        print(f"The user {user_id} is about to be deleted!")
+        manager.disconnect(user_id=user_id)
 
 
             
